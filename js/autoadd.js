15043aa27e63dd50f771a47ca3677fa9688de3a1
Evil Sourcerer <evilsourcerer@gmail.com>
2019-08-09 17:49:17 -0400

    add images.json back

--- js/autoadd.js
+++ js/autoadd.js
@@ -1,5 +1,19 @@
+function addCringyImage (URL) {
+  let cringe = document.createElement('img')
+  cringe.src = URL
+  cringe.className =
+    'materialboxed responsive-img card initialized cringe waves-effect waves-light waves-block'
+
+  let cringecontainer = document.createElement('div')
+  cringecontainer.className = 'col s12 m4 14'
+  cringecontainer.appendChild(cringe)
+  document.getElementById('imagecontainer').appendChild(cringecontainer)
+
+  M.Materialbox.init(cringe)
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-  M.AutoInit();
+  M.AutoInit()
 })
 
 fetch('https://api.github.com/repos/evilsourcerer/impactcringe/contents/images')
@@ -10,21 +24,36 @@ fetch('https://api.github.com/repos/evilsourcerer/impactcringe/contents/images')
     document.getElementById('preload').style.display = 'none'
 
     for (let i = 0; i < res.length; i++) {
-      let cringe = document.createElement('img')
-      cringe.src = res[i]["download_url"]
-      cringe.className = 'materialboxed responsive-img card initialized cringe waves-effect waves-light waves-block'
-
-      let cringecontainer = document.createElement('div')
-      cringecontainer.className = 'col s12 m4 14'
-      cringecontainer.appendChild(cringe)
-      document.getElementById('imagecontainer').appendChild(cringecontainer)
-
-      M.Materialbox.init(cringe);
-      M.Waves.init(cringe);
+      addCringyImage(res[i]['download_url'])
     }
 
-    if(res.length == 0 || res["documentation_url"]) {
-      document.getElementById('imagecontainer').innerHTML = `<h5 style="text-align: center"><i>For some reason no cringe was found...</i></h5>`;
+    if (res.length == 0 || res['documentation_url']) {
+      document.getElementById(
+        'imagecontainer'
+      ).innerHTML = `<h5 style="text-align: center"><i>For some reason no cringe was found...</i></h5>`
+    }
+  })
+  .catch(e => {
+    document.getElementById('preload').innerHTML =
+      'Unfortunately, an error prevented the cringe from loading...'
+    console.log(e)
+  })
+
+fetch('./images.json')
+  .then(res => {
+    return res.json()
+  })
+  .then(res => {
+    document.getElementById('preload').style.display = 'none'
+
+    for (let i = 0; i < res.length; i++) {
+      addCringyImage(res[i])
+    }
+
+    if (res.length == 0) {
+      document.getElementById(
+        'imagecontainer'
+      ).innerHTML = `<h5 style="text-align: center"><i>For some reason no cringe was found...</i></h5>`
     }
   })
   .catch(e => {
